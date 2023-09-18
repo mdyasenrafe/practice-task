9ca1ced42dbf8d7b55f184882b5b70d02ba78b2c
mdyasenrafe <mdyasenrafe@gmail.com>
2023-09-18 17:36:00 +0600

    file setup page working

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  const [loaded] = useFonts({
+    regular: require("./assets/fonts/Satoshi-Regular.otf"),
+    meduim: require("./assets/fonts/Satoshi-Medium.otf"),
+    bold: require("./assets/fonts/Satoshi-Bold.otf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
